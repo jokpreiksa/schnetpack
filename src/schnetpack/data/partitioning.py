89c613819a05ef431e67534da769dcc11ddf67f0
jokubaspreiksa <jokpreiksa@gmail.com>
2023-01-30 13:48:25 +0200

    4th version of xtb Weighted symmetry functions NN: xtbGates, xtbGatedNN, train_test_split2

--- src/schnetpack/data/partitioning.py
+++ src/schnetpack/data/partitioning.py
@@ -4,12 +4,12 @@ from schnetpack.data import AtomsDataError, AtomsDataSubset
 
 
 def train_test_split(
-    data,
-    num_train=None,
-    num_val=None,
-    split_file=None,
-    stratify_partitions=False,
-    num_per_partition=False,
+        data,
+        num_train=None,
+        num_val=None,
+        split_file=None,
+        stratify_partitions=False,
+        num_per_partition=False,
 ):
     """
     Splits the dataset into train/validation/test splits, writes split to
@@ -68,14 +68,14 @@ def train_test_split(
             for start, stop in partitions.values():
                 idx = np.random.permutation(np.arange(start, stop))
                 train_idx += idx[:num_train_part].tolist()
-                val_idx += idx[num_train_part : num_train_part + num_val_part].tolist()
-                test_idx += idx[num_train_part + num_val_part :].tolist()
+                val_idx += idx[num_train_part: num_train_part + num_val_part].tolist()
+                test_idx += idx[num_train_part + num_val_part:].tolist()
 
         else:
             idx = np.random.permutation(len(data))
             train_idx = idx[:num_train].tolist()
-            val_idx = idx[num_train : num_train + num_val].tolist()
-            test_idx = idx[num_train + num_val :].tolist()
+            val_idx = idx[num_train: num_train + num_val].tolist()
+            test_idx = idx[num_train + num_val:].tolist()
 
         if split_file is not None:
             np.savez(
@@ -107,3 +107,126 @@ def create_subset(dataset, indices):
             "The subset indices do not match the total length of the dataset!"
         )
     return AtomsDataSubset(dataset, indices)
+
+
+def split_list_with_indexes(lst, n):
+    result_lists = []
+    result_indexes = []
+    len_list = len(lst)
+    for i in range(0, len(lst), n):
+        if i + n < len_list:
+            result_indexes.append(list(range(i, i + n)))
+        else:
+            new_n = len_list - i
+            result_indexes.append(list(range(i, i + new_n)))
+    return result_indexes
+
+
+def train_test_split2(
+        data,
+        num_train=None,
+        num_val=None,
+        split_file=None,
+        stratify_partitions=False,
+        num_per_partition=False,
+        nth=None,
+):
+    """
+    Function works similarly to train_test_split however this version enables user to split files by taking n entries
+    in the row to the same split, which is useful if data set has n conformers of the same compound. (Made for QMugs DB)
+
+    Args:
+        data (spk.data.AtomsData): full atomistic dataset
+        num_train (int): number of training examples
+        num_val (int): number of validation examples
+        split_file (str): Path to split file. If file exists, splits will
+                          be loaded. Otherwise, a new file will be created
+                          where the generated split is stored.
+        nth (int): splits the data keeping n consecutive elements in the same list. (useful
+        for data sets with number(nth) of different conformers)
+    Returns:
+        spk.data.AtomsDataSubset: subset with training data
+        spk.data.AtomsDataSubset: subset with validation data
+        spk.data.AtomsDataSubset: subset with test data
+
+    """
+
+    if nth is not None or nth == 1:
+        orig_data = data
+        # print(len(orig_data))
+        data = split_list_with_indexes(data, nth)
+        # print(len(orig_data), len(data))
+
+    else:
+        pass
+    if split_file is not None and os.path.exists(split_file):
+        S = np.load(split_file)
+        train_idx = S["train_idx"].tolist()
+        val_idx = S["val_idx"].tolist()
+        test_idx = S["test_idx"].tolist()
+    else:
+        if num_train is None or num_val is None:
+            raise ValueError(
+                "You have to supply either split sizes (num_train /"
+                + " num_val) or an npz file with splits."
+            )
+
+        assert num_train + num_val <= len(
+            data
+        ), "Dataset is smaller than num_train + num_val!"
+
+        num_train = num_train if num_train > 1 else num_train * len(data)
+        num_val = num_val if num_val > 1 else num_val * len(data)
+        num_train = int(num_train)
+        num_val = int(num_val)
+
+        if stratify_partitions:
+            partitions = data.get_metadata("partitions")
+            n_partitions = len(partitions)
+            if num_per_partition:
+                num_train_part = num_train
+                num_val_part = num_val
+            else:
+                num_train_part = num_train // n_partitions
+                num_val_part = num_val // n_partitions
+
+            train_idx = []
+            val_idx = []
+            test_idx = []
+            for start, stop in partitions.values():
+                idx = np.random.permutation(np.arange(start, stop))
+                train_idx += idx[:num_train_part].tolist()
+                val_idx += idx[num_train_part: num_train_part + num_val_part].tolist()
+                test_idx += idx[num_train_part + num_val_part:].tolist()
+
+        else:
+            idx = np.random.permutation(len(data))
+            train_idx = idx[:num_train].tolist()
+            val_idx = idx[num_train: num_train + num_val].tolist()
+            test_idx = idx[num_train + num_val:].tolist()
+
+        if split_file is not None:
+            np.savez(
+                split_file, train_idx=train_idx, val_idx=val_idx, test_idx=test_idx
+            )
+
+    if nth is not None or nth != 1:
+        # print(train_idx)
+        orig_train_idx = [idx for i in train_idx for idx in data[i]]
+        print('Train:', len(orig_data), max(orig_train_idx))
+        train = create_subset(orig_data, orig_train_idx)
+
+        orig_val_idx = [idx for i in val_idx for idx in data[i]]
+        print('Val:', len(orig_data), max(orig_val_idx))
+        val = create_subset(orig_data, orig_val_idx)
+
+        orig_test_idx = [idx for i in test_idx for idx in data[i]]
+        print('Test:', len(orig_data), max(orig_test_idx))
+        test = create_subset(orig_data, orig_test_idx)
+
+    else:
+        train = create_subset(data, train_idx)
+        val = create_subset(data, val_idx)
+        test = create_subset(data, test_idx)
+
+    return train, val, test
